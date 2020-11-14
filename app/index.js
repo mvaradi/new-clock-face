a7649e85e858159d278a068f8cc93204a13d33f3
mvaradi <mvaradi@ebi.ac.uk>
2020-11-14 17:42:58 +0000

    Adding date and battery charge level

--- app/index.js
+++ app/index.js
@@ -1,25 +1,17 @@
-import clock from "clock";
+import battery from "power";
 import document from "document";
-import { preferences } from "user-settings";
-import * as util from "../common/utils";
+import * as customClock from "./clock";
 
-// Update the clock every minute
-clock.granularity = "minutes";
+let txtTime = document.getElementById("txtTime");
+let txtDate = document.getElementById("txtDate");
+let txtBattery = document.getElementById("battery");
 
-// Get a handle on the <text> element
-const myLabel = document.getElementById("myLabel");
-
-// Update the <text> element every tick with the current time
-clock.ontick = (evt) => {
-  let today = evt.date;
-  let hours = today.getHours();
-  if (preferences.clockDisplay === "12h") {
-    // 12h format
-    hours = hours % 12 || 12;
-  } else {
-    // 24h format
-    hours = util.zeroPad(hours);
-  }
-  let mins = util.zeroPad(today.getMinutes());
-  myLabel.text = `${hours}:${mins}`;
+/* --------- CLOCK ---------- */
+function clockCallback(data) {
+    txtTime.text = data.time;
+    txtDate.text = data.date;
 }
+customClock.initialize("minutes", "longDate", clockCallback);
+
+/* -------- BATTERY --------- */
+txtBattery.text = Math.floor(battery.battery.chargeLevel) + "%";
